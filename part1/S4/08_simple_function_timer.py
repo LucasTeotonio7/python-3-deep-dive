40ba32dc4ca2eb49d3bd9d5a9179dda29527ba69
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2025-09-01 21:12:28 -0300

    feat: adds code about args and time variations by function

---
+++ part1/S4/08_simple_function_timer.py
@@ -0,0 +1,67 @@
+import time
+
+def time_it(fn, *args, **kwargs):
+    print(args, kwargs)
+
+time_it(print, 1, 2, 3, sep=" - ", end=" ***")
+
+def time_it(fn, *args, **kwargs):
+    fn(args, kwargs)
+
+time_it(print, 1, 2, 3, sep=" - ", end=" ***")
+
+def time_it(fn, *args, **kwargs):
+    fn(*args, **kwargs)
+
+time_it(print, 1, 2, 3, sep=" - ", end=" ***")
+
+def time_it(fn, *args, rep=1, **kwargs):
+    for i in range(rep):
+        fn(*args, **kwargs)
+
+time_it(print, 1, 2, 3, sep=" - ", end=" ***\n", rep=6)
+
+def time_it(fn, *args, rep=1, **kwargs):
+    start = time.perf_counter()
+    for i in range(rep):
+        fn(*args, **kwargs)
+    end = time.perf_counter()
+    return (end - start) / rep
+
+print(time_it(print, 1, 2, 3, sep=" - ", end=" ***\n", rep=6))
+
+print("\n ------------------- \n")
+
+def compute_powers_1(n, *, start=1, end):
+    # using a for loop
+    results = []
+    for i in range(start, end):
+        results.append(n**i)
+    return results
+
+print(compute_powers_1(5, end=3))
+print(compute_powers_1(5, end=10))
+
+print("\n ------------------- \n")
+
+def compute_powers_2(n, *, start=1, end):
+    # using a list comprehension
+    return [n**i for i in range(start, end)]
+
+print(compute_powers_1(2, end=10))
+
+def compute_powers_3(n, *, start=1, end):
+    # using a generator expression
+    return (n**i for i in range(start, end))
+
+print(compute_powers_3(2, end=7))
+print(list(compute_powers_3(2, end=7)))
+
+print(time_it(compute_powers_1, 2, end=20000, rep=5))
+print(time_it(compute_powers_2, 2, end=20000, rep=5))
+print(time_it(compute_powers_3, 2, end=20000, rep=5))
+
+a = (2**i for i in range(5))
+print(a)
+print(list(a))
+
